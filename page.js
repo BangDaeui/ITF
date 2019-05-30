8e0ac016aaede72d34ad1cc692db3eb8eea72ea0
laida9 <48949984+laida9@users.noreply.github.com>
2019-05-30 19:33:29 +0900

    190530_김규진_2
    Systempolicy, Systempolicy_Manage 시스템 권한에 대한 리스트 페이지와 관리 페이지를 만들었습니다.

--- page.js
+++ page.js
@@ -52,16 +52,74 @@ app.get('/Eventlog', (req, res) => {
 
 app.get('/Users', (req, res) => {
     res.render('Users');
+    
 })
 
 app.get('/Systempolicy', (req, res) => {
-    res.render('Systempolicy');
+    var sql1 = 'select Policy_No, Policy_Name, Policy_Comment, Policy_Update from Policy';
+    conn.query(sql1, function(err, tmp, fields){
+        res.render('Systempolicy', {
+            tmp:tmp      
+        });
+    });
 })
 
-app.get('/Folderpolicymanage', (req, res) => {
-    res.render('Folderpolicymanage');
+app.get('/Systempolicy_Manage/:policyid', (req, res) => {
+    var policyid = req.params.policyid;
+    var sql1 = 'select * from Policy where Policy_No=?';
+        conn.query(sql1, [policyid], function(err, tmp, fields){
+        res.render('Systempolicy_Manage', {
+            tmp:tmp      
+        });
+    });
+    //res.render('Systempolicy_Manage');
 })
 
+app.post('/Systempolicy_Manage',(req,res)=>{
+    var Policy_Name = req.body.Policy_Name;
+    var Policy_Mask = 0;
+    var Policy_Taskmgr = req.body.taskmgr;
+    var Policy_Regedit = req.body.regedit;
+    var Policy_Cmd = req.body.cmd;
+    var Policy_Snipping = req.body.snipping;
+    var Policy_Usbwrite = req.body.usbwrite;
+    var Policy_Usbaccess = req.body.usbaccess;
+    var Policy_Disk = req.bocy.disk;
+    var Policy_Clipboard = req.body.clipboard;
+    
+    if(Policy_Taskmgr == 1){
+        Policy_Mask += 1;
+    }
+    if(Policy_Regedit == 1){
+        Policy_Mask += 2;
+    }
+    if(Policy_Cmd == 1){
+        Policy_Mask += 4;
+    }
+    if(Policy_Snipping == 1){
+        Policy_Mask += 8;
+    }
+    if(Policy_Write == 1){
+        Policy_Mask += 16;
+    }
+    if(Policy_Access == 1){
+        Policy_Mask += 32;
+    }
+    if(Policy_Disk == 1){
+        Policy_Mask += 64;
+    }
+    if(Policy_Clipboard == 1){
+        Policy_Mask += 128;
+    }
+    
+    var sql = 'update Policy set Policy_Mask=?, Policy_Taskmgr=?, Policy_Regedit=?, Policy_Cmd=?, Policy_Snipping=?, Policy_Usbwrite=?, Policy_Usbaccess=?, Policy_Disk=?, Policy_Clipboard=?';
+    
+    conn.query(sql,[Policy_Mask, Policy_Control, Policy_Taskmgr, Policy_Regedit, Policy_Cmd, Policy_Snipping, Policy_Usbwrite, Policy_Usbaccess, Policy_Clipboard, ],function(err, tmp, fields){
+    console.log(tmp);
+    res.redirect('/Systempolicy');
+    });
+    
+})
 app.get('/Folderpolicy', (req, res) => {
     res.render('Folderpolicy');
 })
