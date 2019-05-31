ed945edf5a042b46517d410eaa3d1759982a21e2
laida9 <48949984+laida9@users.noreply.github.com>
2019-05-31 19:48:50 +0900

    190531_김규진_4
    Folderpolicy와 Folderpolicymanage를 연결했습니다.

--- page.js
+++ page.js
@@ -64,7 +64,7 @@ app.get('/Systempolicy', (req, res) => {
         });
     });
 })
-// System Policy control page
+// System Policy Control Page
 app.get('/Systempolicymanage/:policyno', (req, res) => {
     var policyno = req.params.policyno;
     // 시스템 정책 세부 정책 Policy_No를 이용해 구분하며 검색
@@ -125,8 +125,50 @@ app.post('/UpdateSystemPolicy/:policyno', (req, res) => {
     });
     
 })
+
+// Folder Policy list
 app.get('/Folderpolicy', (req, res) => {
-    res.render('Folderpolicy');
+    // 폴더 정책에 대한 데이터 베이스 정보 모두 가져오기
+    var sql1 = 'select * from Folder';
+    conn.query(sql1, function(err, dirpolicy, fields){
+        res.render('Folderpolicy', {
+            dirpolicy:dirpolicy      
+        });
+    });
+});
+
+// Folder Policy Control Page
+app.get('/Folderpolicymanage/:policyno', (req, res) => {
+    // policyno = Folder_No
+    var policyno = req.params.policyno;
+    // 폴더 정책을 Folder_No을 이용하여 가져온다.
+    var sql1 = 'select * from Folder where Folder_No=?';
+    conn.query(sql1, [policyno], function(err, dirpolicy, fields){
+        res.render('Folderpolicymanage', {
+            dirpolicy:dirpolicy      
+        });
+    });
+})
+
+// Update Folder Policy
+app.post('/UpdateFolderPolicy/:policyno', (req, res) => {
+    var policyno = req.params.policyno;
+    var Folder_Name = req.body.Folder_Name;
+    var Folder_Comment = req.body.Folder_Comment;
+    var Folder_Update = req.body.Folder_Update;
+    var Folder_Readonly = req.body.Folder_Readonly;
+    var Folder_Writeable = req.body.Folder_Writeable;
+    var Folder_Guest = req.body.Folder_Guest;
+    var Folder_Browsable = req.body.Folder_Browsable;
+    var Folder_Createmask = req.body.Folder_Createmask;
+    var Folder_Directorymask = req.body.Folder_Directorymask;
+
+    // 폴더 정책 업데이트
+    var sql1 = 'update Folder set Folder_Name=?, Folder_Comment=?, Folder_Update=?, Folder_Readonly=?, Folder_Writeable=?, Folder_Guest=?, Folder_Browsable=?, Folder_Createmask=?, Folder_Directorymask=? where Folder_No=?';
+    conn.query(sql1,[Folder_Name, Folder_Comment, Folder_Update, Folder_Readonly, Folder_Writeable, Folder_Guest, Folder_Browsable, Folder_Createmask, Folder_Directorymask, policyno], function(err, tmp, fields){
+        console.log(tmp);
+        res.redirect('/Folderpolicy');
+    });
 })
 
 app.get('/Setting', (req, res) => {
